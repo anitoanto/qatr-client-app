ce63e9535bbba72d1c7ff3ea8ab94a3083aadc4e
Anito Anto <anitoperamangalam@gmail.com>
2022-04-05 14:56:25 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import pitchAnalyser from "pitch-analyser";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [pitch, setPitch] = useState(0);
+  const [pitchList, setPitchList] = useState([]);
+
+  let freqList = [];
+
+  const analyser = new pitchAnalyser({
+    callback: function (payload) {
+      let freq = payload.frequency;
+      setPitch(freq);
+      setPitchList((pitchList) => [...pitchList, freq]);
+      freqList.push(freq);
+    },
+  });
+
+  function decodePitchList() {
+    // pitchList contained raw sampled data of 3 levels, 3500, 2700, 3100.
+    // convert pitchList into levels avoiding noise values in between.
+    let levels = [];
+    let prev = 0;
+    for (let i = 0; i < freqList.length; i++) {
+      let curr = freqList[i];
+      if (curr !== prev) {
+        levels.push(curr);
+        prev = curr;
+      }
+    }
+    return levels;
+  }
+
+  const handleClick = () => {
+    analyser.initAnalyser().then(() => {
+      analyser.startAnalyser();
+      setTimeout(() => {
+        console.log("stop");
+        // analyser.stopAnalyser();
+        handleStopClick();
+        console.log(decodePitchList());
+      }, 4000);
+    });
+  };
+
+  const handleStopClick = () => {
+    analyser.initAnalyser().then(() => {
+      analyser.stopAnalyser();
+      setPitch(0);
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Quick Audience Turnout Recorder - Group 11</h1>
+      <div className="pitch">{pitch}</div>
+      <div className="btns">
+        <button onClick={handleClick} className="start">Start</button>
+        <button onClick={handleStopClick} className="stop">Reset</button>
+      </div>
+      <div>{pitchList.toString()}</div>
     </div>
   );
 }
